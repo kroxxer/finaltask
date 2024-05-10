a67c28c7c41fd15e94d6ce9e05ac7f31239815c7
Aboba <valikcrazy@gmail.com>
2024-05-10 14:23:03 +0300

    AAAA

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/', name: 'app_default')]
+
 class DefaultController extends AbstractController
 {
+    #[Route('/', name: 'app_default')]
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
