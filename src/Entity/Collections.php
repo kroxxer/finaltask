4c5ef20f59aa9fbd83cab10f039806eb38cbd5ae
Aboba <valikcrazy@gmail.com>
2024-05-20 15:15:23 +0300

    doc

--- src/Entity/Collections.php
+++ src/Entity/Collections.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CollectionsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CollectionsRepository::class)]
@@ -13,10 +15,58 @@ class Collections
     #[ORM\Column]
     private ?int $id = null;
 
-
     private Items $item;
+
+    #[ORM\Column]
+    private bool $custom_string1_state;
+    #[ORM\Column]
+    private string $custom_string1_title;
+    #[ORM\Column]
+    private bool $custom_string2_name;
+
+    #[ORM\Column]
+    private bool $custom_string3_state;
+
+    /**
+     * @var Collection<int, CustomAtributes>
+     */
+    #[ORM\ManyToMany(fetch: 'EAGER', targetEntity: CustomAtributes::class, mappedBy: 'collection')]
+    #[ORM\JoinColumn(nullable: false)]
+    private Collection $customAtributes;
+
+    public function __construct()
+    {
+        $this->customAtributes = new ArrayCollection();
+    }
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, CustomAtributes>
+     */
+    public function getCustomAtributes(): Collection
+    {
+        return $this->customAtributes;
+    }
+
+    public function addCustomAtribute(CustomAtributes $customAtribute): static
+    {
+        if (!$this->customAtributes->contains($customAtribute)) {
+            $this->customAtributes->add($customAtribute);
+            $customAtribute->addCollection($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCustomAtribute(CustomAtributes $customAtribute): static
+    {
+        if ($this->customAtributes->removeElement($customAtribute)) {
+            $customAtribute->removeCollection($this);
+        }
+
+        return $this;
     }
 }
---
+++ src/Entity/CustomAtributes.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomAtributesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CustomAtributesRepository::class)]
+class CustomAtributes
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?bool $custom_string_state = null;
+
+    #[ORM\Column]
+    private ?bool $custom_int_state = null;
+
+    #[ORM\Column]
+    private ?bool $custom_date_state = null;
+
+    #[ORM\Column]
+    private ?bool $custom_bool_state = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $custom_bool_value = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $custom_int_value = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $custom_string_value = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $custom_date_value = null;
+
+    /**
+     * @var Collection<int, Collections>
+     */
+    #[ORM\ManyToMany(fetch: 'EAGER',targetEntity: Collections::class, inversedBy: 'customAtributes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private Collection $collection;
+
+    public function __construct()
+    {
+        $this->collection = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function isCustomStringState(): ?bool
+    {
+        return $this->custom_string_state;
+    }
+
+    public function setCustomStringState(bool $custom_string_state): static
+    {
+        $this->custom_string_state = $custom_string_state;
+
+        return $this;
+    }
+
+    public function isCustomIntState(): ?bool
+    {
+        return $this->custom_int_state;
+    }
+
+    public function setCustomIntState(bool $custom_int_state): static
+    {
+        $this->custom_int_state = $custom_int_state;
+
+        return $this;
+    }
+
+    public function isCustomDateState(): ?bool
+    {
+        return $this->custom_date_state;
+    }
+
+    public function setCustomDateState(bool $custom_date_state): static
+    {
+        $this->custom_date_state = $custom_date_state;
+
+        return $this;
+    }
+
+    public function isCustomBoolState(): ?bool
+    {
+        return $this->custom_bool_state;
+    }
+
+    public function setCustomBoolState(bool $custom_bool_state): static
+    {
+        $this->custom_bool_state = $custom_bool_state;
+
+        return $this;
+    }
+
+    public function isCustomBoolValue(): ?bool
+    {
+        return $this->custom_bool_value;
+    }
+
+    public function setCustomBoolValue(?bool $custom_bool_value): static
+    {
+        $this->custom_bool_value = $custom_bool_value;
+
+        return $this;
+    }
+
+    public function getCustomIntValue(): ?int
+    {
+        return $this->custom_int_value;
+    }
+
+    public function setCustomIntValue(?int $custom_int_value): static
+    {
+        $this->custom_int_value = $custom_int_value;
+
+        return $this;
+    }
+
+    public function getCustomStringValue(): ?string
+    {
+        return $this->custom_string_value;
+    }
+
+    public function setCustomStringValue(?string $custom_string_value): static
+    {
+        $this->custom_string_value = $custom_string_value;
+
+        return $this;
+    }
+
+    public function getCustomDateValue(): ?\DateTimeInterface
+    {
+        return $this->custom_date_value;
+    }
+
+    public function setCustomDateValue(?\DateTimeInterface $custom_date_value): static
+    {
+        $this->custom_date_value = $custom_date_value;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Collections>
+     */
+    public function getCollection(): Collection
+    {
+        return $this->collection;
+    }
+
+    public function addCollection(Collections $collection): static
+    {
+        if (!$this->collection->contains($collection)) {
+            $this->collection->add($collection);
+        }
+
+        return $this;
+    }
+
+    public function removeCollection(Collections $collection): static
+    {
+        $this->collection->removeElement($collection);
+
+        return $this;
+    }
+}
