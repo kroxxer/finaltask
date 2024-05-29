e659b7826890b45b38ee7b2cb6b18eec11e0c743
Aboba <valikcrazy@gmail.com>
2024-05-29 11:49:37 +0300

    a

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +11,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->redirectToRoute('admin');
+        dump($entityManager->createQuery("SELECT * FROM user WHERE TO_TSVECTOR(email) @@ TO_TSQUERY('a@a.a') LIMIT 10;")->getSQL());
+        $emails = $entityManager->createQuery("SELECT * FROM user WHERE TO_TSVECTOR(email) @@ TO_TSQUERY('a@a.a') LIMIT 10;")->getResult();
+        return $this->render("default/index.html.twig", [
+            'emails' => $emails
+            ]
+        );
     }
 }
